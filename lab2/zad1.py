01a78fb7a22193dd21bdf7d5dd5fe19a9f084de5
JacekKajdan <47155575+JacekKajdan@users.noreply.github.com>
2023-10-11 11:47:23 +0200

    Add files via upload

---
+++ lab2/zad1.py
@@ -0,0 +1,7 @@
+import random as rd
+tab = []
+for i in range(100):
+    tab.append(rd.randint(0,100))
+print(tab)
+tab.reverse()
+print(tab)
---
+++ lab2/zad2.py
@@ -0,0 +1,9 @@
+x = int(input("Podaj dzisiejszą temperaturę w stopniach Celcjusza: "))
+if(x<-10):
+    print("Mróz")
+elif(x<10):
+    print("Zimno")
+elif(x<20):
+    print("Ciepło")
+else:
+    print("Gorąco")
